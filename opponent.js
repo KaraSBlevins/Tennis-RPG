3216793bf81c75dd3ba26790c4ba4bca9cc21b88
Kara Blevins <KaraSBlevins@gmail.com>
2017-04-06 02:37:54 -0400

    Add files via upload

--- opponent.js
+++ opponent.js
@@ -2,35 +2,21 @@ function makeOpponent() //creates a random opponent with random stats
 {
 	var firstNames = ["Donald", "Tommy", "John", "James", "Fred", "Pete", "David"];
 	var lastNames = ["Young", "Edwards", "Jackson", "Hartman", "Douglas", "Adams", "Peterson"];
-	var age = [19, 20, 21, 22, 27, 34, 41];
-	var weight = [150, 155, 160, 165, 170, 180, 185];
-	var height = [66, 68, 72, 73, 75, 77, 80];
-	var forehand = [60, 63, 65, 67, 70, 73, 75];
-	var backhand = [58, 62, 67, 70, 71, 76, 80];
-	var serve = [59, 63, 68, 71, 75, 76, 81];
-	var volley = [45, 49, 51, 55, 57, 61, 77];
 
 	var rand1 = Math.floor(Math.random() * firstNames.length);
 	var rand2 = Math.floor(Math.random() * lastNames.length);
-	var rand3 = Math.floor(Math.random() * age.length);
-	var rand4 = Math.floor(Math.random() * weight.length);
-	var rand5 = Math.floor(Math.random() * height.length);
-	var rand6 = Math.floor(Math.random() * forehand.length);
-	var rand7 = Math.floor(Math.random() * backhand.length);
-	var rand8 = Math.floor(Math.random() * serve.length);
-	var rand9 = Math.floor(Math.random() * volley.length);
 	
 	var opponent = //create a new opponent object
 	{
 		firstName: firstNames[rand1],
 		lastName: lastNames[rand2],
-		age: age[rand3],
-		weight: weight[rand4],
-		height: height[rand5],
-		forehand: forehand[rand6],
-		backhand: backhand[rand7],
-		serve: serve[rand8],
-		volley: volley[rand9]
+		age: randomInt(18,45),
+		weight: randomInt(150,220),
+		height: randomInt(66,80),
+		forehand: randomInt(60,80),
+		backhand: randomInt(60,80),
+		serve: randomInt(60,80),
+		volley: randomInt(60,80)
 	};
 	return opponent;
 }
@@ -46,4 +32,11 @@ function displayOpponent(opponent)
 
 var newOpponent = makeOpponent();
 displayOpponent(newOpponent);
+
+function randomInt(min, max) 
+{
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
